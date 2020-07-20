09e230367c1aad5584aa09a6cf07196ebab439dc
Kevin J. Kelly <kevinjkelly@lewisu.edu>
2020-07-20 15:47:56 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function goHome() {
+	clearAll();
+	let home = document.getElementById("home-div");
+	home.style.display = "block";
+}
+function goResume() {
+	clearAll();
+	let resume = document.getElementById("resume-div");
+	resume.style.display = "block";
+}
+function clearAll() {
+	let all = document.getElementsByClassName("content-div");
+	for (var i=0; i<all.length; i++) {
+		all[i].style.display = "none";
+	}
+}
